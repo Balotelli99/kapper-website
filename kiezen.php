fe6fff007b383fb64fe0e26b3482f8b02bdd6284
Balotelli99 <sultanavdi06@gmail.com>
2025-10-28 15:51:34 +0100

    28 oktb

---
+++ kiezen.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Behandelingen Overzicht</title>
+    <link rel="stylesheet" href="behandeloverzicht.css">
+</head>
+<body>
+<h1>Behandelingen/team Overzicht</h1>
+<a href="behandelingen_overzicht.php">Nieuwe Behandeling Toevoegen</a>
+<a href="team_overzicht.php">Nieuwe team Toevoegen</a>
