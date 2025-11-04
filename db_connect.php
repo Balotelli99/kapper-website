e551b761e4d26ae2f1fc2b0799d9e8270a70580b
Balotelli99 <sultanavdi06@gmail.com>
2025-11-04 15:19:29 +0100

    4 nov live zetten

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "kapperwebshop";
+$username = "u240504_kapperwebshop";
+$password = "Wnn9H9E77vDUe4gZVMbp";
+$dbname = "u240504_kapperwebshop";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
